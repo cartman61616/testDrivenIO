cb0241313c13d73f1892da21d8206bb5816457d2
Jonathan Davis <cartman61616@gmail.com>
2019-08-02 08:01:20 -0400

    Finish section 1 and begin section 2. Adds code coverage and cmd level linting

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -134,20 +134,26 @@ class TestUserService(BaseTestCase):
             self.assertEqual(response.status_code, 200)
             self.assertEqual(len(data['data']['users']), 2)
             self.assertIn('robo', data['data']['users'][0]['username'])
-            self.assertIn('atomic_robo@tesladyne.com', data['data']['users'][0]['email'])
+            self.assertIn('atomic_robo@tesladyne.com',
+                          data['data']['users'][0]['email'])
             self.assertIn('marty', data['data']['users'][1]['username'])
-            self.assertIn('goalie30@devils.com', data['data']['users'][1]['email'])
+            self.assertIn('goalie30@devils.com',
+                          data['data']['users'][1]['email'])
             self.assertIn('success', data['status'])
 
     def test_main_no_users(self):
-        """Ensure the main route behaves correctly when no users have been added to the database"""
+        """Ensure the main route behaves
+        correctly when no users
+        have been added to the database"""
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'All Users', response.data)
         self.assertIn(b'<p>No users!</p>', response.data)
 
     def test_main_with_users(self):
-        """Ensure the main route behaves correctly when users have been added to the database"""
+        """Ensure the main route
+        behaves correctly
+        when users have been added to the database"""
         add_user('robo', 'atomic_robo@tesladyne.com')
         add_user('marty', 'goalie30@devils.com')
         with self.client:
