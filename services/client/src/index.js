63993a51dd0ec20b078a2142add145b548a48043
Jonathan Davis <cartman61616@gmail.com>
2019-08-08 08:07:33 -0400

    Fix prod error

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -22,7 +22,7 @@ class App extends Component {
     }
 
     getUsers() {
-        axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+        axios.get(`/users`)
             .then((res) => { this.setState({ users: res.data.data.users}); })
             .catch((err) => { console.log(err); });
     }
@@ -33,7 +33,7 @@ class App extends Component {
             username: this.state.username,
             email: this.state.email
         };
-        axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+        axios.post(`/users`, data)
             .then((res) => {
                 this.getUsers();
                 this.setState({ username: '', email: ''});
