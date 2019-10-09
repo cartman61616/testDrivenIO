ced386743c3f815f38cd832ec8704cfff771b800
Jonathan Davis <cartman61616@gmail.com>
2019-10-09 12:51:31 -0400

    Add react router with About page

--- services/client/src/app.jsx
+++ services/client/src/app.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route, Switch } from 'react-router-dom';
 
 import UsersList from "./components/UsersList";
 import AddUser from "./components/AddUser";
+import About from './components/About';
 
 class App extends Component {
     constructor() {
@@ -49,16 +51,23 @@ class App extends Component {
                     <div className="columns">
                         <div className="column is-half">
                             <br/>
-                            <h1 className="title is-1">All Users</h1>
-                            <hr/><br/>
-                            <AddUser
-                                username={this.state.username}
-                                email={this.state.email}
-                                addUser={this.addUser}
-                                handleChange={this.handleChange}
-                            />
-                            <br/><br/>
-                            <UsersList users={this.state.users}/>
+                            <Switch>
+                                <Route exact path='/' render={() => (
+                                   <div>
+                                        <h1 className="title is-1">All Users</h1>
+                                        <hr/><br/>
+                                        <AddUser
+                                            username={this.state.username}
+                                            email={this.state.email}
+                                            addUser={this.addUser}
+                                            handleChange={this.handleChange}
+                                        />
+                                        <br/><br/>
+                                        <UsersList users={this.state.users}/>
+                                   </div>
+                                )} />
+                                <Route exact path='/about' component={About}/>
+                            </Switch>
                         </div>
                     </div>
                 </div>
